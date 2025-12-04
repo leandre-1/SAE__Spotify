4cfe0ff3b10ee651b017600ca84dd4b5a789c486
Léandre <150375559+leandre-1@users.noreply.github.com>
2025-12-04 11:38:54 +0100

    Add constructor and toString method to Musique class

--- Musique.java
+++ Musique.java
@@ -3,7 +3,7 @@ package SAE_spotify;
 import java.time.LocalDate;
 
 public class Musique {
-    String titre;
+    String titre_Musique;
     String id_titre;
     int duree_titre;
     String type_album;
@@ -12,4 +12,21 @@ public class Musique {
     String nom_Artiste;
     int popularite;
     String artiste;
+
+    public Musique(String titre, String id, int duree, String type, String nomAlb, LocalDate date, String nomArt, int pop, String art) {
+        titre_Musique = titre;
+        id_titre = id;
+        duree_titre = duree;
+        type_album = type;
+        nom_Album = nomAlb;
+        date_album = date;
+        nom_Artiste = nomArt;
+        popularite = pop;
+        artiste = art;
+    }
+
+    public String toString(){
+        return ("Titre : "+titre_Musique+", id du titre : "+id_titre+", Duree du titre : "+duree_titre+", Genre de l'album : "+type_album+
+                ", Nom de l'abum : "+nom_Album+", Nom de l'artiste : "+nom_Artiste+", Popularité : "+popularite+"\n");
+    }
 }
