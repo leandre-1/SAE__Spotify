c7a8c0d31732e0638644e2e30d7a72627112b17e
Léandre <150375559+leandre-1@users.noreply.github.com>
2025-12-04 11:49:52 +0100

    Refactor AppliMusique for improved functionality

--- AppliMusique.java
+++ AppliMusique.java
@@ -1,13 +1,8 @@
 package SAE_spotify;
 
-
-
-
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -30,8 +25,7 @@ public class AppliMusique {
             ListMusique = new LinkedList<>();
         }
         Scanner scanner = new Scanner(System.in);
-        int choix = 0, choix_charge;
-
+        int choix = -1, choix_charge;
         System.out.println("Que voulez vous faire ?");
         System.out.println("1 - Charger les données (choix du fichier)");
         System.out.println("2 - Afficher les données");
@@ -41,81 +35,94 @@ public class AppliMusique {
 
         choix = scanner.nextInt();
 
-        switch(choix){
-            case 1:
-                System.out.println("Vous avez choisi de Charger les données");
-                System.out.println("");
-                System.out.println("Quel fichier voulez-vous charger ?");
-                System.out.println("1 - spotify_100.csv");
-                System.out.println("2 - spotify_1000.csv");
-                System.out.println("3 - spotify_10000.csv");
-                System.out.println("4 - spotify_100000.csv");
-                System.out.println("5 - spotify_FULL.csv");
+       
+            System.out.println("Que voulez vous faire ?");
+            System.out.println("1 - Charger les données (choix du fichier)");
+            System.out.println("2 - Afficher les données");
+            System.out.println("3 - Trier les données");
+            System.out.println("4 - Filtrer les données");
+            System.out.println("5 - Rechercher des données concernant un titre");
+            System.out.println("0 - Quitter");
 
-                choix_charge = scanner.nextInt();
+            choix = scanner.nextInt();
 
-                switch(choix_charge){
-                    case 1:
-                        System.out.println("Chargement du fichier : spotify_100.csv");
-                        Lecture("spotify_100.csv", ARRAYlist);
-                        break;
-                    case 2:
-                        System.out.println("Chargement du fichier : spotify_1000.csv");
-                        Lecture("spotify_1000.csv", ARRAYlist);
-                        break;
-                    case 3:
-                        System.out.println("Chargement du fichier : spotify_10000.csv");
-                        Lecture("spotify_10000.csv", ARRAYlist);
-                        break;
-                    case 4:
-                        System.out.println("Chargement du fichier : spotify_100000.csv");
-                        Lecture("spotify_100000.csv", ARRAYlist);
-                        break;
-                    case 5:
-                        System.out.println("Chargement du fichier : spotify_FULL.csv");
-                        Lecture("spotify_FULL.csv", ARRAYlist);
-                        break;
-                }
-                break;
-            case 2:
-                System.out.println("Vous avez choisi d'afficher les données");
-                Lecture("spotify_100.csv", ARRAYlist);
 
-                break;
-            case 3:
-                System.out.println("Vous avez choisi de Trier les données");
-                System.out.println("");
-                System.out.println("Quel type de tri voulez-vous faire ?");
-                System.out.println("1 - Tri Sélection");
-                System.out.println("2 - Tri Fusion");
-                System.out.println("3 - Tri Java");
-                break;
-            case 4:
-                System.out.println("Vous avez choisi de Filter les données");
-                System.out.println("");
-                System.out.println("Quel type de filtre voulez-vous faire ?");
-                System.out.println("1 - Filtre manuel");
-                System.out.println("2 - Filtre java");
-                break;
-            case 5:
-                System.out.println("Vous avez choisi de Rechercher des données concernant un titre");
-                System.out.println("");
-                System.out.println("Entrez le titre que vous recherchez : ");
-                String titrerecherche = scanner.nextLine();
-                break;
-            default:
-                System.out.println("");
-                System.out.println("Erreur : Veuillez saisir un nombre ente 1 et 5");
-                break;
-        }
+            switch(choix){
+                case 1:
+                    System.out.println("Vous avez choisi de Charger les données");
+                    System.out.println("");
+                    System.out.println("Quel fichier voulez-vous charger ?");
+                    System.out.println("1 - spotify_100.csv");
+                    System.out.println("2 - spotify_1000.csv");
+                    System.out.println("3 - spotify_10000.csv");
+                    System.out.println("4 - spotify_100000.csv");
+                    System.out.println("5 - spotify_FULL.csv");
+
+
+                    choix_charge = scanner.nextInt();
+
+                    switch(choix_charge){
+                        case 1:
+                            System.out.println("Chargement du fichier : spotify_100.csv");
+                            Lecture("spotify_100.csv");
+
+                            break;
+                        case 2:
+                            System.out.println("Chargement du fichier : spotify_1000.csv");
+                            Lecture("spotify_1000.csv");
+                            break;
+                        case 3:
+                            System.out.println("Chargement du fichier : spotify_10000.csv");
+                            Lecture("spotify_10000.csv");
+                            break;
+                        case 4:
+                            System.out.println("Chargement du fichier : spotify_100000.csv");
+                            Lecture("spotify_100000.csv");
+                            break;
+                        case 5:
+                            System.out.println("Chargement du fichier : spotify_FULL.csv");
+                            Lecture("spotify_FULL.csv");
+                            break;
+                    }
+                    break;
+                case 2:
+                    System.out.println("Vous avez choisi d'afficher les données");
+                    Afficher();
+                    break;
+                case 3:
+                    System.out.println("Vous avez choisi de Trier les données");
+                    System.out.println("");
+                    System.out.println("Quel type de tri voulez-vous faire ?");
+                    System.out.println("1 - Tri Sélection");
+                    System.out.println("2 - Tri Fusion");
+                    System.out.println("3 - Tri Java");
+                    break;
+                case 4:
+                    System.out.println("Vous avez choisi de Filter les données");
+                    System.out.println("");
+                    System.out.println("Quel type de filtre voulez-vous faire ?");
+                    System.out.println("1 - Filtre manuel");
+                    System.out.println("2 - Filtre java");
+                    break;
+                case 5:
+                    System.out.println("Vous avez choisi de Rechercher des données concernant un titre");
+                    System.out.println("");
+                    System.out.println("Entrez le titre que vous recherchez : ");
+                    String titrerecherche = scanner.nextLine();
+                    break;
+                default:
+                    System.out.println("");
+                    System.out.println("Erreur : Veuillez saisir un nombre ente 1 et 5");
+                    break;
+            }
+        
     }
 
     public static void Afficher(){
-        
-        
+        System.out.println(ListMusique.toString());
     }
-    public static void Lecture(String nom, boolean ARRAYlist) {
-        
+    public static void Lecture(String nom) {
+
         try{
 
             DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss 'UTC'");
