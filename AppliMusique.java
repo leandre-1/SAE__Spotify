5cfa7ef24f7fd0563e45664b08cc76417cfc12db
Léandre <150375559+leandre-1@users.noreply.github.com>
2025-12-16 09:54:51 +0100

    Refactor AppliMusique with enhanced features

--- AppliMusique.java
+++ AppliMusique.java
@@ -1,11 +1,14 @@
 package SAE_spotify;
 
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URI;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.List;
 
 
 public class AppliMusique {
@@ -13,7 +16,6 @@ public class AppliMusique {
     private static String[] titre;
     private static int[] tabPopularite;
     private static List<Musique> ListMusique;
-    private static long nombreLignes;
     private static String nom;
 
     public static void main(String[] args) {
@@ -57,6 +59,7 @@ public class AppliMusique {
                         case 1:
                             System.out.println("Chargement du fichier : spotify_100.csv");
                             Chargement("spotify_100.csv");
+
                             break;
                         case 2:
                             System.out.println("Chargement du fichier : spotify_1000.csv");
@@ -102,7 +105,7 @@ public class AppliMusique {
                             break;
                         case 2:
                             start = System.nanoTime();
-                            triFusion(0, (int) (nombreLignes - 1));
+                            triFusion(0, (int) (ListMusique.size() - 1));
                             stop = System.nanoTime();
                             Afficher();
                             System.out.println(stop - start + " ms");
@@ -215,14 +218,24 @@ public class AppliMusique {
                     System.out.println("1 - Recherche linéaire");
                     System.out.println("2 - Recherche dichotomique");
 
-                    String phrase = scanner.nextLine();
 
-                    switch(phrase){
+                    choix = scanner.nextInt();
+
+                    switch(choix) {
                         case 1:
+                            System.out.println("Quel titre recherchez-vous ?");
+                            String phrase = "";
+                            do {
+                                phrase = scanner.nextLine();
+                            } while (phrase.isEmpty());
                             RechercheLineaire(phrase);
                             break;
                         case 2:
-                            RechercheDichotomique();
+                            phrase = "";
+                            do {
+                                phrase = scanner.nextLine();
+                            } while (phrase.isEmpty());
+                            RechercheDichotomique(phrase);
                             break;
                     }
                     break;
@@ -236,15 +249,15 @@ public class AppliMusique {
 
 
     public static void Chargement(String nom) {
-
+        SuppressionDUrgenceLente();
         try {
             DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss 'UTC'");
             BufferedReader tsvReader = new BufferedReader(new FileReader(nom));
             String row = new String();
             row = tsvReader.readLine(); //row est égal à null si on est en fin de fichier
             titre = row.split(","); // prends toutes les categories meme les pas necessaires
-            titre = titre[0], titre[1],titre[4],titre[7],titre[9],titre[5]
-                    row = tsvReader.readLine();
+
+            row = tsvReader.readLine();
 
             while (row != null) {
 
@@ -265,14 +278,64 @@ public class AppliMusique {
                 ListMusique.add(musique);
                 row = tsvReader.readLine();
             }
-            nombreLignes = ListMusique.size();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void RechercheDichotomique(){
+    private static void RechercheDichotomique(String phrase){
+        boolean est_triee = false;
+        int choix = 2;
+        int debut = 0;
+        int fin = ListMusique.size() - 1;
+        int m = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Pour faire une recherche Dichotomique, veuillez vous assurer que la liste est triée par titre de musique");
+        System.out.println("1 - La liste est triée");
+        System.out.println("0 - La liste n'est pas triée");
+        do {
+            choix = scanner.nextInt();
+        }while(choix== 2);
+            if(choix==1){
+                est_triee = true;
+            }
+        choix = 2;
+        if (est_triee) {
 
+            while (debut <= fin) {
+                m = (debut + fin) / 2;
+                String PotentielleMusique = ListMusique.get(m).nom_Musique;
+                int resultat = PotentielleMusique.compareTo(phrase);
+                if (resultat == 0) {
+                    System.out.println("Trouvé à l'index : " + m);
+                    System.out.println(ListMusique.get(m));
+                    System.out.println("Voulez vous lire le morceau ?");
+                    System.out.println("0 - Non");
+                    System.out.println("1 - Oui");
+                    do {
+                        choix = scanner.nextInt();
+                    }while(choix== 2);
+                    if(choix==1){
+                        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+                            try {
+                                Desktop.getDesktop().browse(new URI("https://open.spotify.com/intl-fr/track/"+ListMusique.get(m).id_Musique));
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                    break;
+                }
+
+                else if (resultat < 0) {
+                    debut = m + 1;
+                }
+
+                else {
+                    fin = m - 1;
+                }
+            }
+        }
     }
 
     private static void RechercheLineaire(String titre){
@@ -295,31 +358,52 @@ public class AppliMusique {
             ListMusique.remove(0);
             i++;
         }
-        System.out.println(i+"lignes supprimées avec succes");
+        System.out.println(i+" lignes supprimées avec succes");
     }
 
     public static void Afficher() {
         int compteur = 0;
-        int step = (nombreLignes > 1000) ? 1000 : 1;
+        int step = (ListMusique.size() > 1000) ? 1000 : 1;
 
         // On s'assure de ne pas dépasser la taille de la liste
-        int max = Math.toIntExact(Math.min(nombreLignes, ListMusique.size()));
-        System.out.println(Arrays.toString(titre));
+        int max = Math.toIntExact(Math.min(ListMusique.size(), ListMusique.size()));
+        //System.out.println(Arrays.toString(titre));
+        System.out.printf(
+                "%-150s %-25s %-15s %-40s %-150s %-30s %-30s %-20s%n",
+                titre[0],  // nom_Musique
+                titre[1],  // id_Musique
+                titre[2],  // duree
+                titre[3],  // type_album
+                titre[4],  // nom_Album
+                titre[5],  // date
+                titre[6],  // nom_Artiste
+                titre[7]   // popularite
+        );
         for (int i = 0; i < max; i += step) {
-            System.out.println("1. "+ListMusique.get(i));
+            //System.out.println(". "+ListMusique.get(i));
+
+            System.out.printf("%-150s %-25s %-15d %-40s %-150s %-30s %-30s %-20d%n",
+            ListMusique.get(i).nom_Musique,
+            ListMusique.get(i).id_Musique,
+            ListMusique.get(i).duree_titre,
+            ListMusique.get(i).type_album,
+            ListMusique.get(i).nom_Album,
+            ListMusique.get(i).date_album,
+            ListMusique.get(i).nom_Artiste,
+            ListMusique.get(i).popularite);
             compteur++;
         }
         System.out.println(" ");
-        System.out.println(compteur + " lignes affichées sur " + nombreLignes);
+        System.out.println(compteur + " lignes affichées sur " + ListMusique.size());
     }
 
     public static void triSelection() {
         Musique temp;
         int min;
 
-        for (int i = 0; i < nombreLignes - 1; i++) {
+        for (int i = 0; i < ListMusique.size() - 1; i++) {
             min = i;
-            for (int j = i + 1; j < nombreLignes - 1; j++)
+            for (int j = i + 1; j < ListMusique.size() - 1; j++)
                 if (ListMusique.get(min).popularite > ListMusique.get(j).popularite) {
                     min = j;
                 }
